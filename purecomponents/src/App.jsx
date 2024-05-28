9065c560b17011db0d6b67e3dbd69814f71ed3c7
Ankita Laxmi <74234178+Ankita-tech-26@users.noreply.github.com>
2024-05-28 15:38:08 +0530

    Added

---
+++ purecomponents/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+import SimpleCounterComponent from './components/SimpleCounterComponent';
+import PureCounterComponent from './components/PureCounterComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleCounterComponent />
+      <PureCounterComponent />
+    </div>
+  );
+}
+
+export default App;
+
